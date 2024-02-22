196da1767dc2ac44efdcbef86cc8eecca314e3c3
BARBARIK12345 <ktotre@gmail.com>
2024-02-22 19:13:49 +0530

    Footer completed , Homepage animation added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
 
 const Footer = () => {
   return (
     <>
-      <Box bgColor={"blackAlpha.900"} w={"full"} minh={"48"} px={'16'} py={['16','8']} color={'white'}>
+      <Box
+        bgColor={"blackAlpha.900"}
+        w={"full"}
+        minh={"48"}
+        px={"16"}
+        py={["16", "8"]}
+        color={"white"}
+      >
+         <Stack direction={["column", "row"]} h={"full"} alignItems={"center"}>
+        <VStack w={"full"} alignItems={["center", "flex-start"]}>
+          <Text fontWeight={"bold"}>About Us</Text>
+          <Text
+            fontSize={"sm"}
+            letterSpacing={"widest"}
+            textAlign={["center", "left"]}
+          >
+            We are the best crypto trading app in India, we provide our guidance
+            at a very cheap price.
+          </Text>
+        </VStack>
 
+        <VStack>
+          <Avatar boxSize={"28"} mt={["4", "0"]} src={''} />
+          <Text>Our Founder</Text>
+        </VStack>
+      </Stack>
       </Box>
     </>
   );
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,14 +1,28 @@
 import { Box, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 import btcSrc from "../asset/btc.png";
+import { motion } from "framer-motion";
 
 
 const Homepage = () => {
   return (
     <>
       <Box bgColor={'blackAlpha.900'} w={'full'} h={'85vh'}>
-
+      <motion.div
+        style={{
+          height: "80vh",
+        }}
+        animate={{
+          translateY: "20px",
+        }}
+        transition={{
+          duration: 2,
+          repeat: Infinity,
+          repeatType: "reverse",
+        }}
+      >
       <Image w={'full'} h={'full'} objectFit={'contain'} src={btcSrc}/>
+      </motion.div>
       </Box>
       <Text fontSize={'6xl'} textAlign={'center'} fontWeight={'thin'} color={'white'} mt={'-20'}>Xcrypto</Text>
 
